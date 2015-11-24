ee27c0b2a83eab9bbdeb2161a0e213dc03ba9ec0
Borys <lokacyjny@gmail.com>
2015-11-24 10:09:59 +0100

    zmiana wywołania
    Zmiana wywołania gammu-smsd-inject

--- OnReceive.sh
+++ OnReceive.sh
@@ -23,16 +23,16 @@ fi
 if [ -f /var/lib/alarm/rozbrojony ];
 then
   echo "Wysylam SMS ROZBROJONY..." | /usr/bin/logger
-#  echo "Rozbrojony" | /usr/bin/gammu-smsd-inject TEXT $from
+  echo "Rozbrojony" | /usr/bin/gammu-smsd-inject TEXT $from
 # /usr/bin/gammu-smsd-inject TEXT +48600260624 -text "ALARM ROZBROJONY"
 #  echo "ALARM ROZBROJONY" | /usr/bin/gammu sendsms TEXT "$from"
-  /var/spool/alarm/rozbrojony.sh
+# /var/spool/alarm/rozbrojony.sh
 else
   echo "Wysylam SMS UZBBROJONY..." | /usr/bin/logger
-#  echo "Uzbrojony" | /usr/bin/gammu-smsd-inject TEXT $from
+  echo "Uzbrojony" | /usr/bin/gammu-smsd-inject TEXT $from
 #  /usr/bin/gammu-smsd-inject TEXT +48600260624 -text "ALARM UZBROJONY"
 #  echo "ALARM UZBROJONY" | /usr/bin/gammu sendsms TEXT "$from"
-  /var/spool/alarm/uzbrojony.sh
+# /var/spool/alarm/uzbrojony.sh
 fi
 
 echo "KONCZE" | /usr/bin/logger
