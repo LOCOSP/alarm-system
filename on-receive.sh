f689bd51e246f95bf3c25fad238ca696aacbd5cc
Thomas Szteliga <ts@websafe.pl>
2015-11-26 13:01:30 +0100

    Rozbudowane o ponga i uptime

--- on-receive.sh
+++ on-receive.sh
@@ -3,21 +3,38 @@ from=$SMS_1_NUMBER
 message="$SMS_1_TEXT"
 
 #
-if [ "$message" = "ROZBRAJAM" ];
+if [ "$message" = "Disarm" ];
 then
-  echo "Dostalem ROZBRAJAM, rozbrajam alarm..." | /usr/bin/logger
-  /bin/date 1> /tmp/rozbrojony 2> /dev/null
+  echo "Dostalem Disarm - rozbrajam alarm..." | /usr/bin/logger
+  /bin/date 1> /tmp/disarmed 2> /dev/null
+elif [ "$message" = "Arm" ];
+then
+  echo "Dostalem Arm - uzbrajam alarm..." | /usr/bin/logger
+  /bin/rm -f /tmp/disarmed 1> /dev/null 2> /dev/null
+elif [ "$message" = "Ping" ];
+then
+  echo "Dostalem Ping - odpowiadam Pong..." | /usr/bin/logger
+  /usr/bin/gammu-smsd-inject \
+    TEXT $from -text "Pong: $(uptime)" \
+    | /usr/bin/logger
 else
-   echo "Dostalem COKOLWIEK, uzbrajam alarm..." | /usr/bin/logger
-  /bin/rm -f /tmp/rozbrojony 1> /dev/null 2> /dev/null
+   echo "Dostalem nieznane polecenie - olewam..." | /usr/bin/logger
 fi
 
-#
-if [ -f /tmp/rozbrojony ];
+# Sprawdzam, czy wiadomosc to Arm lub Disarm...
+if echo "$message" | grep -Eq "^(Arm|Disarm)$";
 then
-  echo "Wysylam SMS: ROZBROJONY..." | /usr/bin/logger
-  /usr/bin/gammu-smsd-inject TEXT $from -text "Rozbrojony" | /usr/bin/logger
-else
-  echo "Wysylam SMS: UZBBROJONY..." | /usr/bin/logger
-  /usr/bin/gammu-smsd-inject TEXT $from -text "Uzbrojony" | /usr/bin/logger
+  # Sprawdzam, czy istnieje plik /tmp/disarmed...
+  if [ -f /tmp/disarmed ];
+  then
+    echo "Wysylam SMS: ROZBROJONY..." | /usr/bin/logger
+    /usr/bin/gammu-smsd-inject \
+      TEXT $from -text "Alarm rozbrojony: $(uptime)" \
+      | /usr/bin/logger
+  else
+    echo "Wysylam SMS: UZBBROJONY..." | /usr/bin/logger
+    /usr/bin/gammu-smsd-inject \
+      TEXT $from -text "Alarm uzbrojony:  $(uptime)" \
+      | /usr/bin/logger
+  fi
 fi
