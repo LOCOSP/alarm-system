48d4a620e5911a226862208f834c9f20db97a08a
Thomas Szteliga <ts@websafe.pl>
2015-11-26 11:44:41 +0100

    Poprawki o których mówiłem

--- OnReceive.sh
+++ OnReceive.sh
@@ -1,39 +1,23 @@
-#!/bin/sh
-echo OnReceive | /usr/bin/logger
+#!/bin/bash
 from=$SMS_1_NUMBER
 message="$SMS_1_TEXT"
-reply=""
 
 #
-echo "$from" | /usr/bin/logger
-echo "$message" | /usr/bin/logger
-echo "$(whoami)" | /usr/bin/logger
-
-#
-if /usr/bin/test "$message" = "ROZBRAJAM";
+if [ "$message" = "ROZBRAJAM" ];
 then
   echo "Dostalem ROZBRAJAM, rozbrajam alarm..." | /usr/bin/logger
-  /bin/date > /var/lib/alarm/rozbrojony
+  /bin/date 1> /var/lib/alarm/rozbrojony 2> /dev/null
 else
    echo "Dostalem COKOLWIEK, uzbrajam alarm..." | /usr/bin/logger
-  /bin/rm -f /var/lib/alarm/rozbrojony
+  /bin/rm -f /var/lib/alarm/rozbrojony 1> /dev/null 2> /dev/null
 fi
 
 #
 if [ -f /var/lib/alarm/rozbrojony ];
 then
-  echo "Wysylam SMS ROZBROJONY..." | /usr/bin/logger
-  echo "Rozbrojony" | /usr/bin/gammu-smsd-inject TEXT $from
-# /usr/bin/gammu-smsd-inject TEXT +48600260624 -text "ALARM ROZBROJONY"
-#  echo "ALARM ROZBROJONY" | /usr/bin/gammu sendsms TEXT "$from"
-# /var/spool/alarm/rozbrojony.sh
+  echo "Wysylam SMS: ROZBROJONY..." | /usr/bin/logger
+  /usr/bin/gammu-smsd-inject TEXT $from -text Rozbrojony" | /usr/bin/logger
 else
-  echo "Wysylam SMS UZBBROJONY..." | /usr/bin/logger
-  echo "Uzbrojony" | /usr/bin/gammu-smsd-inject TEXT $from
-#  /usr/bin/gammu-smsd-inject TEXT +48600260624 -text "ALARM UZBROJONY"
-#  echo "ALARM UZBROJONY" | /usr/bin/gammu sendsms TEXT "$from"
-# /var/spool/alarm/uzbrojony.sh
+  echo "Wysylam SMS: UZBBROJONY..." | /usr/bin/logger
+  /usr/bin/gammu-smsd-inject TEXT $from -text Uzbrojony" | /usr/bin/logger
 fi
-
-echo "KONCZE" | /usr/bin/logger
-exit 0
